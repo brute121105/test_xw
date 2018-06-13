c55dd4c0d4b6e0c33c658dbf15075df0a1fee922
brute121105 <brute121105@users.noreply.github.com>
2018-06-14 00:30:26 +0800

    2018年6月9日17:13:04

--- app/src/main/java/hyj/xw/util/DaoUtil.java
+++ app/src/main/java/hyj/xw/util/DaoUtil.java
@@ -30,9 +30,9 @@ public class DaoUtil {
         return wx008Datas;
     }
     public static List<Wx008Data> findByDataBydataFlag(){
-         List<Wx008Data> wx008Datas = DataSupport.where("(dataFlag=? or dataFlag=? or dataFlag=?) and wxPwd is not null","007","008","009").order("createTime asc").find(Wx008Data.class);
+         //List<Wx008Data> wx008Datas = DataSupport.where("(dataFlag=? or dataFlag=? or dataFlag=?) and wxPwd is not null","007","008","009").order("createTime asc").find(Wx008Data.class);
 
-        //List<Wx008Data> wx008Datas = DataSupport.where("(dataFlag=? or dataFlag=? or dataFlag=?) and dieFlag=? and (expMsg not like '%帐号%' or expMsg is null)  and wxPwd is not null","007","008","009","999").order("createTime asc").find(Wx008Data.class);
+        List<Wx008Data> wx008Datas = DataSupport.where("(dataFlag=? or dataFlag=? or dataFlag=?) and dieFlag=? and (expMsg not like '%帐号%' or expMsg is null)  and wxPwd is not null","007","008","009","888").order("createTime asc").find(Wx008Data.class);
         //List<Wx008Data> wx008Datas = DataSupport.where("(dataFlag=? or dataFlag=? or dataFlag=?) and dieFlag!=? and (expMsg not like '%帐号%' or expMsg is null)  and wxPwd is not null","007","008","009","999").order("createTime asc").find(Wx008Data.class);
 
         //List<Wx008Data> wx008Datas = DataSupport.where("(dataFlag=? or dataFlag=? or dataFlag=?) and dieFlag=0","007","008","009").order("createTime asc").find(Wx008Data.class);
